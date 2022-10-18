fe1fb726981d9f3ed9714d564938aaac0a07c704
sadik <po480373@gmai.com>
2022-10-18 22:31:45 +0600

    data type int string

---
+++ data-types.dart
@@ -0,0 +1,26 @@
+/* 
+Strongly typed: the type of variable is known at compile time.
+ex: c++, java , swift
+
+Dynamic type:  the type of variable is known at run time.
+ex: python, ruby, js
+
+ */
+main() {
+  /* 
+  --fundamental type---
+  int
+  double
+  String
+  bool
+  -----------------------
+  dynamic (we can change the value at runtime with different type value)
+   */
+  int amount1 = 100; //static type
+  var amount2 = 200; //dynamic type
+  print("amount1: $amount1 | amount2: $amount2 \n");
+
+  String name1 = "Sadik";
+  var name2 = "hossain";
+  print("name1: $name1 | name2: $name2 \n");
+}
