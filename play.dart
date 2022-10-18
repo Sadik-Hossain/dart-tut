017dcea766c52a6a684fe5cfb747ab5466a6c5cd
sadik <po480373@gmai.com>
2022-10-18 22:04:43 +0600

    semicolon is 2 much painful and print()takes 1 argument

--- play.dart
+++ play.dart
@@ -1,1 +1,9 @@
-
+// if main fn doesn't return anything write void
+// void is optional
+// void main (){
+// }
+main() {
+  var firstname = "sadik";
+  String lastName = "hossain";
+  print(firstname + " " + lastName);
+}
