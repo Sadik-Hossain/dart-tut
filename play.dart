2b02bc62953eb7898b00afc06463f00477a2b708
sadik <po480373@gmai.com>
2022-10-18 22:09:12 +0600

    comment added

--- play.dart
+++ play.dart
@@ -1,9 +1,18 @@
+//dart:core | built in package
+// import 'dart:core'; // dart:core will be imported automatically. we dont need to specify manually
+
 // if main fn doesn't return anything write void
 // void is optional
 // void main (){
 // }
+
+// semicolon is 2 much painful and print() takes 1 argument
+//var can be any data type
+// explicitly saying string before variable, makes other data types unassignable
+
 main() {
   var firstname = "sadik";
+  // statically type defined
   String lastName = "hossain";
   print(firstname + " " + lastName);
 }
